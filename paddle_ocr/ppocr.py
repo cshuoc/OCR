ffad92f01b52ecb83aeda1321f706bdbb5928380
cshuoc <163077272+cshuoc@users.noreply.github.com>
2024-03-19 17:37:23 +0800

    ppocr.py

--- paddle_ocr/ppocr.py
+++ paddle_ocr/ppocr.py
@@ -6,8 +6,8 @@ from googletrans import Translator
 from matplotlib import pyplot as plt
 from paddleocr import PaddleOCR,draw_ocr
 
-def pic_to_txt(img_path : str,  result_save_path : str, pic_lang : str, target_lang : str):
-    ppocr = PaddleOCR(lang= pic_lang ,use_gpu=False, use_angle_cls = True)
+def pic_to_txt(img_path : str,  result_save_path : str, pic_lang : str, target_lang : str, gpu:bool):
+    ppocr = PaddleOCR(lang= pic_lang ,use_gpu=gpu, use_angle_cls = True)
     image = cv2.imread(img_path)
     image = cv2.resize(image, (image.shape[0]*2, image.shape[1]*2), interpolation=cv2.INTER_AREA)
     result = ppocr.ocr(image)
@@ -39,7 +39,8 @@ if __name__ == "__main__":
     parser.add_argument('--result_text_path', type=str, default='/your/path/text.txt', help='辨識結果儲存路徑')
     parser.add_argument('--pic_lang', type=str, default='en', help='欲辨識圖像文字類型')
     parser.add_argument('--target_lang', type=str, default='chinese_cht', help='欲辨識圖像文字類型')
+    parser.add_argument('--gpu', type=bool, default=False, help='是否使用gpu')
     
     args = parser.parse_args()    
     
-    pic_to_txt(args.img_path, args.result_text_path, args.pic_lang, args.target_lang)
+    pic_to_txt(args.img_path, args.result_text_path, args.pic_lang, args.target_lang, args.gpu)
